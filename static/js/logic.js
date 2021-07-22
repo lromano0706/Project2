b86c1d4e23519498b2fed75ad383ce0ddb344473
Coco <cocojhjslin9019@gmail.com>
2021-07-21 20:26:10 -0700

    updating html

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,2 +1,35 @@
-// API key
-const API_KEY = "pk.eyJ1IjoiY29jb2poanNsaW45MDE5IiwiYSI6ImNrcXkwcWhrNTEza3Aydm1udjdkbzllNDcifQ.unkvrGAMAA7WMTFThU5X1Q";
+// Creating our initial map object
+// We set the longitude, latitude, and the starting zoom level
+// This gets inserted into the div with an id of 'map'
+var myMap = L.map("map", {
+  center: [35.52, -100.67],
+  zoom: 13
+});
+
+// Adding a tile layer (the background map image) to our map
+// We use the addTo method to add objects to our map
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/satellite-v9",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+
+// // Initialize and add the map
+// function initMap() {
+//   // The location of Uluru
+//   const uluru = { lat: -25.344, lng: 131.036 };
+//   // The map, centered at Uluru
+//   const map = new google.maps.Map(document.getElementById("map"), {
+//     zoom: 4,
+//     center: uluru,
+//   });
+//   // The marker, positioned at Uluru
+//   const marker = new google.maps.Marker({
+//     position: uluru,
+//     map: map,
+//   });
+// }
